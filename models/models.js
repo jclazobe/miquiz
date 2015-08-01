416ad4dc70c0c4a70b0f2571e0f817823616620a
Juan Carlos Lázaro <jclazobe@gmail.com>
2015-08-01 20:59:34 +0200

    Postgress y Heroku
    Se ha incorporado heroku y se ha creado Procfile que no se hizo en su
    momento para que pueda funcionar con foreman en local. SQLite se ha
    movido a las dependencias de desarrollo

--- models/models.js
+++ models/models.js
@@ -1,14 +1,38 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passw@host:port/database
+// SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar Model ORM
 var Sequelize = require('sequelize');
 
-// Usar BD SQLite
-var sequelize = new Sequelize(null,null,null,
-						{dialect:"sqlite",storage:"quiz.sqlite"}
-					);
+// Usar BD SQLite o Postgres (comentar el que no corresponda)
+// Este primer bloque es para postgres
+var sequelize = new Sequelize(DB_name,user,pwd,
+	{ dialect:protocol,  // dialect y protocol son lo mismo por lo que se podría poner dialect:dialect
+	  protocol:protocol,
+	  port:port,
+	  host:host,
+	  storage:storage, // solo SQLite (.env)
+	  omitNull:true    // solo Postgres
+	}
+);
 
-// Importar la definición de la tabla Quiz en qui.js
+// Usar BD SQLite
+/* var sequelize = new Sequelize(null,null,null,
+						{dialect:"sqlite",storage:"quiz.sqlite"}
+					); */
+
+// Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 exports.Quiz = Quiz; // Exportar definición de tabla Quiz
