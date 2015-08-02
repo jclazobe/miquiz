db35691b328ec79653346cd8b1c11eeb941d23de
Juan Carlos Lázaro <jclazobe@gmail.com>
2015-08-02 13:07:59 +0200

    Autenticación y sesión
    Se ha instalado express-session para el control de sesiones y se ha
    añadido enlaces para login (y logout) y una vista específica para login.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,6 +15,7 @@ exports.load = function(req,res,next,quizId){
 	).catch(function(error){next(error);});
 };
 
+
 // GET /quizes   (Todo el listado de preguntas)
 exports.index = function(req,res){
 	var consulta={};
